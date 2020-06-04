8ca258c8ec388ac47f73455fe8db4ff70cc6dd6f
tonia-jam <66325349+tonia-jam@users.noreply.github.com>
2020-06-04 19:49:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let numCount = 10;
+for (let i = 0; i < numCount; i++) {
+    console.log(i);
+};
+numCount = 11;
+for (let i = 1; i < numCount; i++) {
+    console.log(i);
+  };
+  for (let i = 0; i < numCount; i++) {
+    if (i % 2 === 0) {
+    console.log(i);
+    }
+  }
